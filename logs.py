b2f145b5922e3507ba0d06c768750e31778636ad
Toolazyapp <151521936+Toolazyapp@users.noreply.github.com>
2023-12-15 09:52:53 +0100

    Delete logs.py

--- logs.py
+++
@@ -1,5 +0,0 @@
-from tkinter import messagebox
-def error(text,title='Error'):
-	messagebox.showerror(title=title,message=text)
-def info(text,title='Info'):
-	messagebox.showinfo(title=title,message=text)
